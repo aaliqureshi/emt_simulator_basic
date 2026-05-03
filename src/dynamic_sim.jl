8713a4e1b288a6c8e80ee10dac2bbcdc0459e40c
aali27 <aali27@ncsu.edu>
2026-05-03 14:20:53 -0400

    dynamic_sim - make the sytsem in ODE form

--- src/dynamic_sim.jl
+++ src/dynamic_sim.jl
@@ -3,7 +3,7 @@ module DynamicSim
 export solve_dynamic_sim!, build_dynamic_address, build_mass_matrix, build_initial_conditions
 
 include("Models.jl"); using .Models
-include("algebraic_solvers.jl")
+# include("algebraic_solvers.jl")
 
 function solve_dynamic_sim!(du, u, p, t)
     solve_generator!(du, u, p)
@@ -61,12 +61,12 @@ function build_mass_matrix(sys, address)
     mass_matrix = zeros(n_states, n_states)
 
     # bus balance equations (capacitance)
-    # for (i, bus_idx) in enumerate(address["balance_d"])
-    #     real_bus_idx = non_slack_buses[i]
-    #     mass_matrix[bus_idx, bus_idx] = C_eq[real_bus_idx]
-    #     q_idx = address["balance_q"][i]
-    #     mass_matrix[q_idx, q_idx] = C_eq[real_bus_idx]
-    # end
+    for (i, bus_idx) in enumerate(address["balance_d"])
+        real_bus_idx = non_slack_buses[i]
+        mass_matrix[bus_idx, bus_idx] = C_eq[real_bus_idx]
+        q_idx = address["balance_q"][i]
+        mass_matrix[q_idx, q_idx] = C_eq[real_bus_idx]
+    end
 
     # line id (inductance)
     i = 1
@@ -82,7 +82,7 @@ function build_mass_matrix(sys, address)
         i += 1
     end
 
-    # delta
+    # # delta
     for i in collect(address["delta"])
         mass_matrix[i,i] = 1.0
     end
